7ec3cd051b933a4cdcfeee139525ab83cf11da2a
Mringa School <lorna.mikatei@student.moringaschool.com>
2022-09-27 10:27:09 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity= 'roller-skate'){
+  console.log(`This Saturday, I want to ${activity}!`)  
+  return `This Saturday, I want to ${activity}!`
+}
+// SaturdayFun
+
+function mondayWork(doWhat = 'go to the office'){
+
+    console.log(`This Monday, I will  ${doWhat}.`);
+    return `This Monday, I will ${doWhat}.`
+}
+
+function wrapAdjective(part1='*'){
+    // const encouragingPromptFunction =wrapAdjective("!!!")
+    return function(emphatic="special"){
+        return `You are ${part1}${emphatic}${part1}!`
+    }
+}
+
